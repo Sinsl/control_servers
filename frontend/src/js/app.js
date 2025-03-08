b53becc3437d283fd3a1b8b3b7fd5de3debbe582
Sinsl <sinsl@me.com>
2025-03-08 19:46:18 +0300

    chenge yml actions 20

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -3,12 +3,25 @@ import { Controller } from "./Controller";
 console.log("inretface start");
 console.log(process.env.MODE);
 
+const container = document.querySelector(".container");
+
+const msg = document.createElement("div");
+if (process.env.MODE === "production") {
+  msg.className = "msg-start";
+  msg.insertAdjacentHTML(
+    "beforeend",
+    `<p>Сервер на Render запускается</p>
+    <p>Дождитесь завершения</p>`,
+  );
+  container.append(msg);
+}
+
 const res = await fetch(process.env.BASE_URL + "/ping");
 
 if (res.ok) {
   console.log("backend starting");
+  msg.remove();
   // Создание контроллера
-  const container = document.querySelector(".container");
   const controller = new Controller(container);
 
   // Запуск методов последовательно
